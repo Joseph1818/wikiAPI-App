c8bbf664160fd537470a18fc434d86f94519e7c3
Uncle Jee <ujee@UMBAS-MacBook-Air.local>
2023-06-20 10:19:24 +0200

    Third commit, delete method added.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1/wikiDB", { useNewUrlParser: true });
 //TODO
 
-//GET ROUT, this methodes get all items in the articles model and print it.
+//GET ROUT Methods, this methodes get all items in the articles model and print it.
 
 app.get("/articles", function (req, res) {
   Article.find(function (err, foundArticles) {
@@ -42,23 +42,38 @@ app.get("/articles", function (req, res) {
   });
 });
 
-// POST ROUTE
+// POST ROUTE Methods.
 app.post("/articles", function (req, res) {
   console.log();
   console.log();
 
-  //Creating data inside. Mogodb database
+  //Creating data inside.
   const ArticleData = new Article({
     tile: res.body.title,
     content: res.body.content,
   });
-  ArticleData.save(function(err){
+  ArticleData.save(function (err) {
     if (!err) {
-        res.send("Successfuly added a new article.");
+      res.send("Successfuly added a new article.");
     } else {
-        res.send(err);
+      res.send(err);
     }
   });
+});
+
+// DELETE Methods
+app.delete("/articles", function (req, res) {
+  //Delete data methods.
+  ArticleData.deleteMany(
+    ("/articles",
+    function (req, res) {
+      if (!err) {
+        res.send("Deleted succesfully");
+      } else {
+        res.send(err);
+      }
+    })
+  );
 });
 
 app.listen(3000, function () {
