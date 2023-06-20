2a8e170043b79a677d35cbb402d07b0c0a7c435b
Uncle Jee <ujee@UMBAS-MacBook-Air.local>
2023-06-20 10:45:59 +0200

    fourth commit, Chainable route handlers added (It reduces redundancy & all routes are specified at a single locations.)

--- app.js
+++ app.js
@@ -27,54 +27,100 @@ mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1/wikiDB", { useNewUrlParser: true });
 //TODO
 
-//GET ROUT Methods, this methodes get all items in the articles model and print it.
+// Chainable route handlers for a route path using app.route()(Reduces redundancy & specified at a single location).
 
-app.get("/articles", function (req, res) {
-  Article.find(function (err, foundArticles) {
-    // To print it on browser using console log.
-    // console.log(foundArticles);
-    // To print it on browser using res.send
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-// POST ROUTE Methods.
-app.post("/articles", function (req, res) {
-  console.log();
-  console.log();
-
-  //Creating data inside.
-  const ArticleData = new Article({
-    tile: res.body.title,
-    content: res.body.content,
-  });
-  ArticleData.save(function (err) {
-    if (!err) {
-      res.send("Successfuly added a new article.");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-// DELETE Methods
-app.delete("/articles", function (req, res) {
-  //Delete data methods.
-  ArticleData.deleteMany(
-    ("/articles",
-    function (req, res) {
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      // To print it on browser using console log.
+      // console.log(foundArticles);
+      // To print it on browser using res.send
       if (!err) {
-        res.send("Deleted succesfully");
+        res.send(foundArticles);
       } else {
         res.send(err);
       }
-    })
-  );
-});
+    });
+  })
+  .post(function (req, res) {
+    console.log();
+    console.log();
+
+    //Creating data inside.
+    const ArticleData = new Article({
+      tile: res.body.title,
+      content: res.body.content,
+    });
+    ArticleData.save(function (err) {
+      if (!err) {
+        res.send("Successfuly added a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    //Delete data methods.
+    ArticleData.deleteMany(
+      ("/articles",
+      function (req, res) {
+        if (!err) {
+          res.send("Deleted succesfully");
+        } else {
+          res.send(err);
+        }
+      })
+    );
+  });
+
+//GET ROUT Methods, this methodes get all items in the articles model and print it.
+// app.get("/articles", function (req, res) {
+//   Article.find(function (err, foundArticles) {
+//     // To print it on browser using console log.
+//     // console.log(foundArticles);
+//     // To print it on browser using res.send
+//     if (!err) {
+//       res.send(foundArticles);
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+// POST ROUT Methods.
+// app.post("/articles", function (req, res) {
+//   console.log();
+//   console.log();
+
+//   //Creating data inside.
+//   const ArticleData = new Article({
+//     tile: res.body.title,
+//     content: res.body.content,
+//   });
+//   ArticleData.save(function (err) {
+//     if (!err) {
+//       res.send("Successfuly added a new article.");
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+// DELETE Methods
+// app.delete("/articles", function (req, res) {
+//   //Delete data methods.
+//   ArticleData.deleteMany(
+//     ("/articles",
+//     function (req, res) {
+//       if (!err) {
+//         res.send("Deleted succesfully");
+//       } else {
+//         res.send(err);
+//       }
+//     })
+//   );
+// });
 
 app.listen(3000, function () {
   console.log("Server started succesfully on port 3000");
